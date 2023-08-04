501d08ac9014484398b332e89530d9bf28e608a3
KanterIV <Kanterigor@gmail.com>
2023-08-04 13:15:11 +0200

    task #1 is ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.stopBtn.setAttribute('disabled', true);
+
+refs.startBtn.addEventListener('click', onStartButtonClick);
+refs.stopBtn.addEventListener('click', stopBgcChangeTimer);
+
+let timerId;
+
+function onStartButtonClick() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.setAttribute('disabled', true);
+  refs.stopBtn.removeAttribute('disabled');
+}
+
+function stopBgcChangeTimer() {
+  clearInterval(timerId);
+  refs.stopBtn.setAttribute('disabled', true);
+  refs.startBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
